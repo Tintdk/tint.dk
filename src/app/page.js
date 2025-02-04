76ece709c71d7646b28e4b3ff81144090add4818
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-02-04 11:26:10 +0100

    Add smiley emoji to home page greeting

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="text-xl">Hej Frederik!</h1>
+      <h1 className="text-xl">Hej Frederik! &#128516;</h1>
     </div>
   );
 }
