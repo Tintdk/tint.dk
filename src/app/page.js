b3dab4bf67bbefb58561e3e7e656d5aaa5f18406
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-02-17 12:15:02 +0100

    Add HTTPS notification to home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <h1 className="text-xl">Hej Frederik! &#128516;</h1>
+      <p>Nu er der HTTPS</p>
     </div>
   );
 }
