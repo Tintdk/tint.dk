ebf0213f4d1d842709e26d74d948b581d47b4a45
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-06-02 13:38:46 +0200

    chore: update dependencies and refactor Tailwind CSS configuration
    - Added autoprefixer as a dependency.
    - Updated postcss to version 8.5.4 and tailwindcss to version 4.0.0.
    - Refactored postcss.config.mjs to use the new Tailwind CSS plugin format.
    - Removed Tailwind CSS directives from globals.css and replaced them with an import statement.
    - Deleted the tailwind.config.mjs file as its configuration is now handled in globals.css.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Enables static exports
-    reactStrictMode: true,
-    images: {
-        unoptimized: true, // Deactivates because GitHub Pages doesn't support the Next.js image optimization
-      },
-  };
-  
-  export default nextConfig;
+  output: "export", // Enables static exports
+  reactStrictMode: true,
+};
+
+export default nextConfig;
