cf2b0df6b5b27c404161d3874a73e40a686317a8
Tintdk <frederikmark3009@gmail.com>
2025-06-02 21:13:57 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const projects = {
   "test-projekt": {
     title: "125 års jubilæum",
-    description: "I anledning af Praxis’ 125-års jubilæum har jeg udviklet en animationsfilm, der formidler organisationens historie, værdier og udvikling gennem tiden. Projektet omfattede hele den kreative og tekniske proces – fra idéudvikling til færdig film. Jeg har udarbejdet storyboard og koncept, samt stået for udvælgelse og kuratering af relevant arkivmateriale, som blev integreret i animationen. Arkivmaterialet er blevet nøje gennemgået og anvendt som visuelle og historiske elementer i fortællingen. Projektet blev udført i tæt dialog med relevante interessenter med fokus på at skabe en levende og sammenhængende visuel fortælling, der både respekterer Praxis’ arv og kommunikerer organisationens betydning i dag.",
+    description: "I anledning af Praxis’ 125-års jubilæum har jeg udviklet en animationsfilm, der formidler organisationens historie, værdier og udvikling gennem tiden. Projektet omfattede hele den kreative og tekniske proces – fra idéudvikling til færdig film. Jeg har udarbejdet storyboard og koncept, samt stået for udvælgelse og kuratering af relevant arkivmateriale, som er blevet nøje gennemgået og anvendt som visuelle og historiske elementer i fortællingen. Projektet blev udført i tæt dialog med relevante interessenter med fokus på at skabe en levende og sammenhængende visuel fortælling, der både respekterer Praxis’ arv og kommunikerer organisationens betydning i dag.",
     client: "Praxis",
     topic: "Animation",
     services: ["Motion Graphics", "Videoproduktion"],
