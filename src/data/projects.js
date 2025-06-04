5ee2d0d7864a647de736ce4cf67f470ec96b1c93
Tintdk <frederikmark3009@gmail.com>
2025-06-04 21:30:37 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export const projects = {
     services: ["Motion Graphics", "Videoproduktion"],
     cover: {
       type: "image",
-      src: "/media/IMG_4191.jpeg"
+      src: "/media/praxiscover1.jpeg"
     },
     projectMedia: [
       { type: "image", src: "/media/test-image-text-02.png" },
